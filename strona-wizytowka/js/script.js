8255bd91338e7901c0980bff00113db988e34a92
mariansc83 <mariansc83@icloud.com>
2020-12-05 14:23:39 +0100

    Hamburger changed

--- strona-wizytowka/js/script.js
+++ strona-wizytowka/js/script.js
@@ -5,14 +5,14 @@ $(document).ready(function() {
   console.log(hamburger);
 
   hamburger.click(function() {
-    hamburger.addClass('hide');
+    hamburger.hide();
     times.fadeIn(550);
     menuOpen.slideDown(550);
   });
 
   times.click(function() {
       menuOpen.slideUp(550);
-      times.fadeOut();
-      hamburger.removeClass('hide');
+      times.hide();
+      hamburger.fadeIn();
     });
 });
