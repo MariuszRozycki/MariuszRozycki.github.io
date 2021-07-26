e0ba5c14a4200fa901e5c8ceca1a332b1892ec53
MariuszRozycki <mariansc83@icloud.com>
2021-07-26 15:49:22 +0200

    Updates

---
+++ precache-manifest.12da8d8287be47a6123da7dc7ae841c9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "151c9d313d11df666811",
+    "url": "/router-spa-app-second/static/css/main.6976ed6b.chunk.css"
+  },
+  {
+    "revision": "151c9d313d11df666811",
+    "url": "/router-spa-app-second/static/js/main.b06cba13.chunk.js"
+  },
+  {
+    "revision": "acca371ea5890809cc36",
+    "url": "/router-spa-app-second/static/js/runtime~main.b0528e0c.js"
+  },
+  {
+    "revision": "577c7734a4a8cd2ccbfe",
+    "url": "/router-spa-app-second/static/js/2.8c9b526c.chunk.js"
+  },
+  {
+    "revision": "70e6670ee746fc59f414fde4fcce93af",
+    "url": "/router-spa-app-second/static/media/header1.70e6670e.jpg"
+  },
+  {
+    "revision": "33eca35fceea8311d67358a6629fa9d9",
+    "url": "/router-spa-app-second/static/media/header2.33eca35f.jpg"
+  },
+  {
+    "revision": "d9d36b65a132d26aed884ad58e4719eb",
+    "url": "/router-spa-app-second/static/media/header3.d9d36b65.jpg"
+  },
+  {
+    "revision": "ecf91bb3aaa8c40d4bb8fdf7fd617883",
+    "url": "/router-spa-app-second/index.html"
+  }
+];
